baa675baf2b6525d4ba0a509be4614d116952eb0
elegantuniv <korea651@gmail.com>
2017-01-08 21:26:06 +0900

    scaffold to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170105231605) do
+ActiveRecord::Schema.define(version: 20170108121921) do
 
   create_table "links", force: :cascade do |t|
     t.string   "long_url"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20170105231605) do
     t.integer  "user_id"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
